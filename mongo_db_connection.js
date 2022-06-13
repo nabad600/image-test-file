ebfb1f7a71e8cc68201e7f73fc1617e805bafd72
Naba Kumar Das <nabad600@gmail.com>
2022-06-14 00:09:32 +0530

    Update mongo_db_connection.js

--- mongo_db_connection.js
+++ mongo_db_connection.js
@@ -15,33 +15,33 @@
 // 		console.log("Error in the connectivity");
 // })
 
-// const { MongoClient } = require('mongodb')
-// const db = { connected: false }
+const { MongoClient } = require('mongodb')
+const db = { connected: false }
 
-// db.client = new MongoClient('mongodb://mongo:27017/')
-// db.client.on('open', _=>{ db.connected=true, log(now()+'DB connected.') })
-// db.client.on('topologyClosed', _=>{ db.connected=false, log(now()+'DB disconnected.') })
+db.client = new MongoClient('mongodb://mongo:27017/')
+db.client.on('open', _=>{ db.connected=true, log(now()+'DB connected.') })
+db.client.on('topologyClosed', _=>{ db.connected=false, log(now()+'DB disconnected.') })
 
 
-var MongoClient = require('mongodb').MongoClient;
-var util= require('util');
-var encoder = new util.TextEncoder('utf-8');
-// Connect to the db
-MongoClient.connect("mongodb://mongo:27017/MyDb", function (err, db) {
+// var MongoClient = require('mongodb').MongoClient;
+// var util= require('util');
+// var encoder = new util.TextEncoder('utf-8');
+// // Connect to the db
+// MongoClient.connect("mongodb://mongo:27017/MyDb", function (err, db) {
     
-    db.collection('Persons', function (err, collection) {
+//     db.collection('Persons', function (err, collection) {
         
-        collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
-        collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
-        collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
+//         collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
+//         collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
+//         collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
         
         
 
-        db.collection('Persons').count(function (err, count) {
-            if (err) throw err;
+//         db.collection('Persons').count(function (err, count) {
+//             if (err) throw err;
             
-            console.log('Total Rows: ' + count);
-        });
-    });
+//             console.log('Total Rows: ' + count);
+//         });
+//     });
                 
-});
+// });
