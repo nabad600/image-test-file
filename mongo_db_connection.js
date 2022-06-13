bcbb1942d2b01a4cf821092057cc382d21d1248f
Naba Kumar Das <nabad600@gmail.com>
2022-06-13 23:40:33 +0530

    Update mongo_db_connection.js

--- mongo_db_connection.js
+++ mongo_db_connection.js
@@ -15,9 +15,32 @@
 // 		console.log("Error in the connectivity");
 // })
 
-const { MongoClient } = require('mongodb')
-const db = { connected: false }
+// const { MongoClient } = require('mongodb')
+// const db = { connected: false }
 
-db.client = new MongoClient('mongodb://mongo:27017/')
-db.client.on('open', _=>{ db.connected=true, log(now()+'DB connected.') })
-db.client.on('topologyClosed', _=>{ db.connected=false, log(now()+'DB disconnected.') })
+// db.client = new MongoClient('mongodb://mongo:27017/')
+// db.client.on('open', _=>{ db.connected=true, log(now()+'DB connected.') })
+// db.client.on('topologyClosed', _=>{ db.connected=false, log(now()+'DB disconnected.') })
+
+
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect("mongodb://mongo:27017/MyDb", function (err, db) {
+    
+    db.collection('Persons', function (err, collection) {
+        
+        collection.insert({ id: 1, firstName: 'Steve', lastName: 'Jobs' });
+        collection.insert({ id: 2, firstName: 'Bill', lastName: 'Gates' });
+        collection.insert({ id: 3, firstName: 'James', lastName: 'Bond' });
+        
+        
+
+        db.collection('Persons').count(function (err, count) {
+            if (err) throw err;
+            
+            console.log('Total Rows: ' + count);
+        });
+    });
+                
+});
