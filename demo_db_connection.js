51ea365fd729441687e3d575d57ee2bd231d4a4b
Naba Kumar Das <nabad600@gmail.com>
2022-06-13 15:04:36 +0530

    Update demo_db_connection.js

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -6,7 +6,10 @@ var con = mysql.createConnection({
   password: "test"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+connection.connect(function(err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+
+  console.log('Connected to the MySQL server.');
 });
