a55a8887292e0f98beedddfd0687994c8b9965b4
Naba Kumar Das <nabad600@gmail.com>
2022-06-14 00:11:17 +0530

    Update mongo_db_connection.js

--- mongo_db_connection.js
+++ mongo_db_connection.js
@@ -1,26 +1,26 @@
-// // Module calling
-// const MongoClient = require("mongodb");
+// Module calling
+const MongoClient = require("mongodb");
 
-// // Server path
-// const url = 'mongodb://mongo:27017/';
+// Server path
+const url = 'mongodb://mongo:27017/';
 
-// // Name of the database
-// // const dbname = "conFusion";
+// Name of the database
+// const dbname = "conFusion";
 
-// MongoClient.connect(url, (err,client)=>{
-// 	if(!err) {
-// 		console.log("successful connection with the server");
-// 	}
-// 	else
-// 		console.log("Error in the connectivity");
-// })
+MongoClient.connect(url, (err,client)=>{
+	if(!err) {
+		console.log("successful connection with the server");
+	}
+	else
+		console.log("Error in the connectivity");
+})
 
-const { MongoClient } = require('mongodb')
-const db = { connected: false }
+// const { MongoClient } = require('mongodb')
+// const db = { connected: false }
 
-db.client = new MongoClient('mongodb://mongo:27017/')
-db.client.on('open', _=>{ db.connected=true, log(now()+'DB connected.') })
-db.client.on('topologyClosed', _=>{ db.connected=false, log(now()+'DB disconnected.') })
+// db.client = new MongoClient('mongodb://mongo:27017/')
+// db.client.on('open', _=>{ db.connected=true, log(now()+'DB connected.') })
+// db.client.on('topologyClosed', _=>{ db.connected=false, log(now()+'DB disconnected.') })
 
 
 // var MongoClient = require('mongodb').MongoClient;
